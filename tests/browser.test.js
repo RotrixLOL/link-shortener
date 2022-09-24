2553cd59a871d2c61a6da7c6fedcb2ad8dff882f
RotrixxLOL <ncusnir43@gmail.com>
2022-09-24 17:18:33 +0200

    modificate test script

--- tests/browser.test.js
+++ tests/browser.test.js
@@ -5,19 +5,19 @@ describe('Browser testing', () => {
     let page
 
     beforeAll(done => {
-        browser = puppeeter.launch()
-        page = browser.newPage()
+        browser = await puppeeter.launch()
+        page = await browser.newPage()
         done()
     })
 
     it('make a screenshot', done => {
-        page.goto('http://localhost:5173')
-        page.screenshot({ path: 'screenshot.png' })
+        await page.goto('http://localhost:5173')
+        await page.screenshot({ path: 'screenshot.png' })
         done()
     })
 
     afterAll(done => {
-        browser.close()
+        await browser.close()
         done()
     })
 })
